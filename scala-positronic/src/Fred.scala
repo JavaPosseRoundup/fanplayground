82dfe5bff359908aef5c47017bfa5ed5808c654f
Fred Simon <freds@jfrog.com>
2013-02-25 15:29:01 -0700

    Fred compiles and run...

--- scala-positronic/src/Fred.scala
+++ scala-positronic/src/Fred.scala
@@ -1,30 +1,39 @@
-object Recorder extends App {
-	val history = List[Map[String, Int]]()
-    var position = 0
+import collection.mutable
+import scala.collection.mutable.ListBuffer
 
-    def get(fieldname: String) = {
-      	if (history.size <= position) {
-      		// TODO: Copy the map from previous history
-      		history += new Map[String, Int]
-      	}
-      	history(position).get(fieldname)
-    }
+object Recorder {
+  val history = ListBuffer[mutable.ListMap[String, Int]]()
+  var position = 0
 
-    def set(fieldname: String, value: Int) = {
-      	if (history.size <= position) {
-      		// TODO: Copy the map from previous history
-      		history += new Map[String, Int]
-      	}
-      	history(position)(fieldname) = value
+  def get(fieldname: String) = {
+    if (history.size <= position) {
+      // TODO: Copy the map from previous history
+      history += new mutable.ListMap[String, Int]()
     }
+    history(position).get(fieldname)
+  }
+
+  def set(fieldname: String, value: Int) {
+    if (history.size <= position) {
+      // TODO: Copy the map from previous history
+      history += new mutable.ListMap[String, Int]()
+    }
+    history(position)(fieldname) = value
+  }
 }
 
-class Fred {
-	def blah = Recorder.get("blah")
-	def blah_=(x : Int) = { Recorder.set("blah", x) }
+object Fred extends App {
+  def blah = Recorder.get("blah")
+  def blah_=(x: Int) {
+    Recorder.set("blah", x)
+  }
+
+  override def main(args: Array[String]) {
+    blah = 12
+    println(blah)
+    blah = 14
+    println(blah)
+  }
 }
 
-val fred = new Fred()
-fred.blah = 12
-println(fred.blah)
-	
+
