a76d94a4983e176433d0efa6eb8b2d70854b6428
Dianne Marsh <dmarsh@srtsolutions.com>
2013-02-25 15:58:07 -0700

    dianne code on the side

---
+++ scala-positronic/src/Dianne.scala
@@ -0,0 +1,37 @@
+import scala.collection.mutable.MutableList
+import scala.collection.mutable.Map
+
+object Recorder extends App {
+	val history = MutableList[Map[String, Int]]()
+    var position = 0
+
+    def get(fieldname: String) = {
+      	if (history.size <= position) {
+
+      		// TODO: Copy the map from previous history
+      		val m = Map[String, Int]()
+      		m += fieldname -> position
+      		history +=  m
+
+      	}
+      	history(position).get(fieldname)
+    }
+
+    def set(fieldname: String, value: Int) = {
+      	if (history.size <= position) {
+      		// TODO: Copy the map from previous history
+      		history += Map(fieldname->value)
+      	}
+      	history(position)(fieldname)->value
+    }
+}
+
+class Fred {
+	def blah = Recorder.get("blah")
+	def blah_=(x : Int) = { Recorder.set("blah", x) }
+}
+
+val fred = new Fred()
+fred.blah = 12
+println(fred.blah)
+	
