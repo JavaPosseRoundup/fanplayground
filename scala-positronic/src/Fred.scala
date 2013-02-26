5b7ece9e64ed131d0a4e7903d6ed01fafe2c2427
Fred Simon <freds@jfrog.com>
2013-02-26 13:54:53 -0700

    Added QuatumValue class and now it works

--- scala-positronic/src/Fred.scala
+++ scala-positronic/src/Fred.scala
@@ -1,28 +1,49 @@
-import collection.mutable
 import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.HashMap
 
 class QuantumValue[A](val value: Option[A] = None) {
   def get: A = value.get
+
   def isEmpty: Boolean = value.isEmpty
 }
 
 object Universe {
-  val history = ListBuffer[mutable.ListMap[String, QuantumValue[Int]]]()
+  val debug = false
+  val history = ListBuffer[HashMap[String, QuantumValue[Int]]]()
   var position = 0
   var numRun = 0
+  var finalRun = false
 
-  def get(fieldname: String): Option[Int] = {
+  private def checkHistory(fieldname: String) {
     if (history.size <= position) {
       // TODO: Copy the map from previous history
-      history += new mutable.ListMap[String, QuantumValue[Int]]()
+      history += new HashMap[String, QuantumValue[Int]]()
+      history(position) += (fieldname -> new QuantumValue[Int]())
     }
-    history(position).get(fieldname) match {
-      case None => None
-      case Some(quVal) => quVal.value
+  }
+
+  def get(fieldname: String): Option[Int] = {
+    getFromTime(fieldname, position) match {
+      // Let's try get the value from the future
+      case None => getFromTime(fieldname, position + 1)
+      case Some(i) => Some(i)
+    }
+  }
+
+  def getFromTime(fieldname: String, pos: Int): Option[Int] = {
+    checkHistory(fieldname)
+    if (history.size > pos) {
+      history(pos).get(fieldname) match {
+        case None => None
+        case Some(quVal) => quVal.value
+      }
+    } else {
+      None
     }
   }
 
   def addValue(fieldname: String, value: Option[Int]) {
+    checkHistory(fieldname)
     history(position) += (fieldname -> new QuantumValue[Int](value))
     position = position + 1
   }
@@ -32,19 +53,29 @@ object Universe {
   }
 
   def live(func: => Unit) {
-    while (numRun < 20 && !isDone) {
+    while (numRun < 5 && !isDone) {
       position = 0
-      println("in the loop " + numRun)
       func
+      if (debug) {
+        println("in the loop " + numRun + " position=" + position)
+      }
       numRun = numRun + 1
     }
     if (isDone) {
+      finalRun = true
       position = 0
       func
     }
   }
 
   def isDone: Boolean = {
+    if (debug) {
+      println("Debug from is done")
+      for {
+        h <- history
+        p <- h
+      } yield println(s"${h.hashCode()} ${p._1} : ${p._2.value}")
+    }
     numRun != 0 &&
       !history.isEmpty &&
       !history.exists(_.isEmpty) &&
@@ -69,7 +100,7 @@ object Fred extends App {
   }
 
   def out(temp: Any) {
-    if (Universe.isDone) println(temp)
+    if (Universe.finalRun) println(temp)
   }
 
   Universe.live {
