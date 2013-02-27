2dffefd15584c46e79b9a750c004a7fd1bd6bf24
Fred Simon <freds@jfrog.com>
2013-02-26 18:02:23 -0700

    Some refactoring

--- scala-positronic/src/Fred.scala
+++ scala-positronic/src/Fred.scala
@@ -1,107 +1,9 @@
-import scala.collection.mutable.ListBuffer
-import scala.collection.mutable.HashMap
-
-class QuantumValue[A](val value: Option[A] = None) {
-  def get: A = value.get
-
-  def isEmpty: Boolean = value.isEmpty
-}
-
-object Universe {
-  val debug = false
-  val history = ListBuffer[HashMap[String, QuantumValue[Int]]]()
-  var position = 0
-  var numRun = 0
-  var finalRun = false
-
-  private def checkHistory(fieldname: String) {
-    if (history.size <= position) {
-      // TODO: Copy the map from previous history
-      history += new HashMap[String, QuantumValue[Int]]()
-      history(position) += (fieldname -> new QuantumValue[Int]())
-    }
-  }
-
-  def get(fieldname: String): Option[Int] = {
-    getFromTime(fieldname, position) match {
-      // Let's try get the value from the future
-      case None => getFromTime(fieldname, position + 1)
-      case Some(i) => Some(i)
-    }
-  }
-
-  def getFromTime(fieldname: String, pos: Int): Option[Int] = {
-    checkHistory(fieldname)
-    if (history.size > pos) {
-      history(pos).get(fieldname) match {
-        case None => None
-        case Some(quVal) => quVal.value
-      }
-    } else {
-      None
-    }
-  }
-
-  def addValue(fieldname: String, value: Option[Int]) {
-    checkHistory(fieldname)
-    history(position) += (fieldname -> new QuantumValue[Int](value))
-    position = position + 1
-  }
-
-  def set(fieldname: String, value: Option[Int]) {
-    addValue(fieldname, value)
-  }
-
-  def live(func: => Unit) {
-    while (numRun < 5 && !isDone) {
-      position = 0
-      func
-      if (debug) {
-        println("in the loop " + numRun + " position=" + position)
-      }
-      numRun = numRun + 1
-    }
-    if (isDone) {
-      finalRun = true
-      position = 0
-      func
-    }
-  }
-
-  def isDone: Boolean = {
-    if (debug) {
-      println("Debug from is done")
-      for {
-        h <- history
-        p <- h
-      } yield println(s"${h.hashCode()} ${p._1} : ${p._2.value}")
-    }
-    numRun != 0 &&
-      !history.isEmpty &&
-      !history.exists(_.isEmpty) &&
-      !history.exists(_.values.exists(_.isEmpty))
-  }
-}
 
 object Fred extends App {
 
-  val theList = List("now", "is", "the", "time")
-  val toFind = "the"
-  val temperature = 32
-
-  def blah = Universe.get("blah")
-
-  def blah_=(x: Int) {
-    Universe.set("blah", Some(x))
-  }
-
-  def blah_=(x: Option[Int]) {
-    Universe.set("blah", x)
-  }
-
-  def out(temp: Any) {
-    if (Universe.finalRun) println(temp)
-  }
+  val theList = List("now", "is", "the", "time", "to", "find", "in", "a", "list")
+  val toFind = "in"
+  val temperature = 240
 
   Universe.live {
     out(blah)
@@ -112,6 +14,20 @@ object Fred extends App {
     }
     out(blah)
     blah = theList.indexOf(toFind)
+  }
+
+  def blah = Universe.get("blah")
+
+  def blah_=(x: Int) {
+    blah = Some(x)
+  }
+
+  def blah_=(x: Option[Int]) {
+    Universe.set("blah", x)
+  }
+
+  def out(temp: Any) {
+    if (Universe.finalRun) println(temp)
   }
 }
 
--- scala-positronic/src/Positronic.scala
+++ scala-positronic/src/Positronic.scala
@@ -1,3 +1,84 @@
-object PositronicApp extends App {
-	
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.HashMap
+
+class QuantumValue[A](val value: Option[A] = None) {
+  def get: A = value.get
+
+  def isEmpty: Boolean = value.isEmpty
+}
+
+object Universe {
+  val debug = false
+  val history = ListBuffer[HashMap[String, QuantumValue[Int]]]()
+  var position = 0
+  var numRun = 0
+  var finalRun = false
+
+  private def checkHistory(fieldname: String) {
+    if (history.size <= position) {
+      // TODO: Copy the map from previous history
+      history += new HashMap[String, QuantumValue[Int]]()
+      history(position) += (fieldname -> new QuantumValue[Int]())
+    }
+  }
+
+  def get(fieldname: String): Option[Int] = {
+    getFromTime(fieldname, position) match {
+      // Let's try get the value from the future
+      case None => getFromTime(fieldname, position + 1)
+      case Some(i) => Some(i)
+    }
+  }
+
+  def getFromTime(fieldname: String, pos: Int): Option[Int] = {
+    checkHistory(fieldname)
+    if (history.size > pos) {
+      history(pos).get(fieldname) match {
+        case None => None
+        case Some(quVal) => quVal.value
+      }
+    } else {
+      None
+    }
+  }
+
+  def addValue(fieldname: String, value: Option[Int]) {
+    checkHistory(fieldname)
+    history(position) += (fieldname -> new QuantumValue[Int](value))
+    position = position + 1
+  }
+
+  def set(fieldname: String, value: Option[Int]) {
+    addValue(fieldname, value)
+  }
+
+  def live(func: => Unit) {
+    while (numRun < 5 && !isDone) {
+      position = 0
+      func
+      if (debug) {
+        println("in the loop " + numRun + " position=" + position)
+      }
+      numRun = numRun + 1
+    }
+    if (isDone) {
+      finalRun = true
+      position = 0
+      func
+    }
+  }
+
+  def isDone: Boolean = {
+    if (debug) {
+      println("Debug from is done")
+      for {
+        h <- history
+        p <- h
+      } yield println(s"${h.hashCode()} ${p._1} : ${p._2.value}")
+    }
+    numRun != 0 &&
+      !history.isEmpty &&
+      !history.exists(_.isEmpty) &&
+      !history.exists(_.values.exists(_.isEmpty))
+  }
 }
